fa83158529b59667e99e92cc3a1654aab209e1e6
ymCatis <ymkim@catis.biz>
2024-10-14 17:00:00 +0900

    초기 커밋

---
+++ electron.ts
@@ -0,0 +1,33 @@
+import path from 'path'
+import { app, BrowserWindow } from 'electron'
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  if (process.env.NODE_ENV !== 'production') {
+    win.loadURL('http://localhost:5173')
+  } else {
+    win.loadFile(path.join(__dirname, 'dist', 'index.html'))
+  }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
